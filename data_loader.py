0082cd6a86bab2990f3c4f307777d21c847a0f62
Emil Haldan <emilhaldan@live.dk>
2024-03-13 14:19:18 +0100

    load_race_data changed the coords

--- data_loader.py
+++ data_loader.py
@@ -1,19 +1,28 @@
 import pandas as pd
 import numpy as np
 
-def load_track_data(distance_pr_dot:float = 0):
+def load_track_data(distance_pr_dot:float = 0.1):
     track_left_side = pd.read_csv(f'Data/Map_details/nurburgring_GP_track_leftside_raw.csv', index_col=False)
     track_right_side = pd.read_csv(f'Data/Map_details/nurburgring_GP_track_rightside_raw.csv', index_col=False)
 
     track_left_side = track_left_side[["Timestamp","X-Coords", "Y-Coords", "Z-Coords"]]
     track_right_side = track_right_side[["Timestamp","X-Coords", "Y-Coords", "Z-Coords"]]
 
-    track_left_side["Z-Coords"] = track_left_side["Z-Coords"] * -1
-    track_right_side["Z-Coords"] = track_right_side["Z-Coords"] * -1
-    track_left_side["X-Coords"] = track_left_side["X-Coords"] * -1
-    track_right_side["X-Coords"] = track_right_side["X-Coords"] * -1
-    track_left_side["Timestamp"] = track_left_side["Timestamp"] / 1000
+    track_left_side["Z-Coords"]   = track_left_side["Z-Coords"] * -1
+    track_right_side["Z-Coords"]  = track_right_side["Z-Coords"] * -1
+    track_left_side["X-Coords"]   = track_left_side["X-Coords"] * -1
+    track_right_side["X-Coords"]  = track_right_side["X-Coords"] * -1
+    track_left_side["Timestamp"]  = track_left_side["Timestamp"] / 1000
     track_right_side["Timestamp"] = track_right_side["Timestamp"] / 1000
+
+    # Rename columns
+    track_left_side.rename(columns={"X-Coords": "Y-Coords", 
+                                    "Z-Coords": "X-Coords", 
+                                    "Y-Coords": "Z-Coords"}, inplace=True)
+
+    track_right_side.rename(columns={"X-Coords": "Y-Coords", 
+                                     "Z-Coords": "X-Coords", 
+                                     "Y-Coords": "Z-Coords"}, inplace=True)
 
     
     if type(distance_pr_dot) not in [float, int]:
@@ -29,7 +38,7 @@ def load_track_data(distance_pr_dot:float = 0):
         print("\n"*2)
         raise ValueError("Paramter: distance_pr_dot must be positive")
     if distance_pr_dot > 0:
-        track_left_side = reduce_dataframe(track_left_side, distance_pr_dot)
+        track_left_side  = reduce_dataframe(track_left_side,  distance_pr_dot)
         track_right_side = reduce_dataframe(track_right_side, distance_pr_dot)
 
     return track_left_side, track_right_side
@@ -43,8 +52,8 @@ def reduce_dataframe(df, distance_pr_dot):
                 continue
             previous_row = rows[-1]
             if np.sqrt((row['X-Coords'] - previous_row['X-Coords'])**2 + 
-                        (row['Z-Coords'] - previous_row['Z-Coords'])**2 + 
-                        (row['Y-Coords'] - previous_row['Y-Coords'])**2) >= distance_pr_dot:
+                       (row['Z-Coords'] - previous_row['Z-Coords'])**2 + 
+                       (row['Y-Coords'] - previous_row['Y-Coords'])**2) >= distance_pr_dot:
                 rows.append(row)
 
         reduced_df = pd.DataFrame(rows, columns=df.columns)
@@ -55,12 +64,18 @@ def load_race_data(file: str, remove_first_lap: bool = True):
     telemetry_df = pd.read_csv(f'Data/Telemetry_data/{file}', index_col=False)
 
     telemetry_df["Timestamp"] = telemetry_df["Timestamp"] / 1000
-    telemetry_df["LapTime"] = -telemetry_df["Timestamp"].diff()
-    telemetry_df["Z-Coords"] = telemetry_df["Z-Coords"] * -1
-    telemetry_df["X-Coords"] = telemetry_df["X-Coords"] * -1
+    telemetry_df["LapTime"]   = -telemetry_df["Timestamp"].diff()
+    telemetry_df["Z-Coords"]  = telemetry_df["Z-Coords"] * -1
+    telemetry_df["X-Coords"]  = telemetry_df["X-Coords"] * -1
     telemetry_df["NewLap"] = telemetry_df["LapTime"] > 60
     if remove_first_lap:
         telemetry_df = telemetry_df[telemetry_df["Laps Completed"] > 0]
+
+    # Rename columns
+    telemetry_df.rename(columns={"X-Coords": "Y-Coords", 
+                                 "Z-Coords": "X-Coords", 
+                                 "Y-Coords": "Z-Coords"}, inplace=True)
+
 
     telemetry_df = telemetry_df.sort_values(by=["Laps Completed", "Timestamp"])
     telemetry_df.reset_index(drop=True, inplace=True)
